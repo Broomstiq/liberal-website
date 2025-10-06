aa3d8539ec5284ef1efd21d761378ec1972b19f2
Broomstiq <malekamille@gmail.com>
2025-10-06 10:17:20 +0200

    added sanity deploy config

---
+++ sanity.cli.js
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fv1dy8am',
+    dataset: 'production'
+  },
+  studioHost: 'libe-portfolio',
+  deployment: {
+    appId: 'ttwm9ior7bp8t1yjcsdozopc',
+  },
+})
