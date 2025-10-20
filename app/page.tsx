e0ef78e1824ca2030c3356578212a61255e25654
Broomstiq <malekamille@gmail.com>
2025-10-20 12:03:25 +0200

    updated gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,7 @@ export default function Home() {
 
         {/* Projects Mosaïque Grid */}
         <section className="container mx-auto px-4 pb-12">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
             {filteredProjects.map((project) => (
               <ProjectCard key={project._id} project={project} />
             ))}
